b56d4e6e9c56e35bd2fa5768048119afdda60c17
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2018-02-12 04:15:16 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+Bucket List
+============
+
+Aventurous things to do before you kick the bucket here on earth!  Uses **Angular.js**, Bootstrap, and FontAwesome.  Use it to make your **_Bucket List_**. In truth, just another fancy 'To Do' list by a developer that loves to code.
+
+![Bucket List](http://http://ryanhunter.org/portfolio/bucketlist/bucketlist.png)
+
+---
+
+## Features
+- Angular.js framework
+- Bootstrap 3
+- FontAwesome 4 vector icons
+- Google Fonts
+
+
+## License
+
+This project is licensed under the terms of the **MIT** license.
---
+++ bucketlist/bucketlist.js
@@ -0,0 +1,63 @@
+// Bucket List
+var app = angular.module("myApp", []);
+
+app.filter("capitalize", function() {
+	return function(input) {
+		var str = input.charAt(0).toUpperCase();
+		str += input.slice(1);
+		return str;
+	};
+});
+
+app.controller("mainController", [
+	"$scope",
+	function(self) {
+		self.currentTask = { text: "", marked: false, color: "green" };
+
+		self.Tasks = [
+			{ text: "Go Bungee-Jumping", marked: false, color: "blue" },
+			{ text: "Running with the bulls in Spain", marked: false, color: "blue" },
+			{ text: "Run the Toronto Marathon", marked: false, color: "blue" },
+			{ text: "Hang glide over Rio de Janeiro", marked: false, color: "red" },
+			{ text: "Parachute from a Plain", marked: true, color: "gray" },
+			{ text: "Go Para-sailing", marked: true, color: "gray" },
+			{ text: "Raft through the Grand Canyon", marked: false, color: "red" },
+			{ text: "Swim with dolphins in Jamaica", marked: false, color: "black" },
+			{ text: "Swim with whale sharks in the Maldives", marked: false, color: "green" },
+			{ text: "Party Carnival in Jamaica", marked: true, color: "gray" },
+			{ text: "Treetok Treking", marked: true, color: "gray" }
+		];
+
+		self.submitTask = function() {
+			if (self.currentTask.text != "") {
+				self.Tasks.push(self.currentTask);
+				self.currentTask = { text: "", marked: false, color: "gray" };
+			}
+		};
+
+		self.clearTasks = function() {
+			self.Tasks = self.Tasks.filter(function(task) {
+				return !task.marked;
+			});
+		};
+
+		self.markTask = function(task) {
+			task.marked = !task.marked;
+		};
+		self.color_id = 0;
+		self.changeTaskColor = function(task) {
+			// in priority order (reverse)
+			// 1. black, 2. red,..., last: gray
+			var colors = ["gray", "green", "blue", "red", "black"];
+			if (!task.marked) {
+				if (self.color_id < 4) {
+					self.color_id += 1;
+				} else {
+					self.color_id = 0;
+				}
+				task.color = colors[self.color_id];
+			}
+		};
+	}
+]);
+
